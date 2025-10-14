15dfedcd9862eedcaa884228a09cd1f437edff97
Emmanuel Vernet <vernet.emmanuel@gmail.com>
2025-10-14 22:22:06 +0200

    testing controller actions ongoing

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,9 +6,15 @@ class PagesController < ApplicationController
 
   # Inertia
   def inertia_home
-    render inertia: "InertiaHome", props: {
-      worlds: World.includes(:user, :tags)
-    }
+    # render inertia: "InertiaHome", props: {
+    #   worlds: World.includes(:user, :tags)
+    # }
+    #   Rails.logger.info props.as_json
+
+    worlds = World.includes(:user, :tags)
+    # Rails.logger.info worlds.as_json
+    render inertia: "InertiaHome", props: { worlds: worlds }
+    raise
   end
 end
 
